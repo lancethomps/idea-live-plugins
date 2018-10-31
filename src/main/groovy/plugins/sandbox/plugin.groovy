debcd178f8f2fe1c2e7bd2656e28e167d72440d3
Lance Thompson <lthompson05@gmail.com>
2018-10-30 22:04:02 -0700

    misc

--- src/main/groovy/plugins/sandbox/plugin.groovy
+++ src/main/groovy/plugins/sandbox/plugin.groovy
@@ -1,23 +1,23 @@
-import static liveplugin.PluginUtil.*
+package plugins.sandbox
+
+import static liveplugin.PluginUtil.log
+import static liveplugin.PluginUtil.show
 
 import com.intellij.ide.plugins.PluginManager
 import com.intellij.openapi.editor.colors.impl.AppEditorFontOptions
-
-import common.PrefsUtil
-
 // add-to-classpath $HOME/github/idea-live-plugins/src/main/groovy
 
+if (isIdeStartup) return
 
 static def showEditorFontSize() {
   show("Editor Font Size: ${AppEditorFontOptions.getInstance().getState().FONT_SIZE}")
 }
 
 static def logPlugins() {
-  def pluginIds = PluginManager.getPlugins().collect {it.getPluginId()}
+  def pluginIds = PluginManager.getPlugins().collect { it.getPluginId() }
   pluginIds.sort()
   pluginIds.each { log(it) }
 }
 
-PrefsUtil.hello()
-//showEditorFontSize()
+showEditorFontSize()
 //logPlugins()
