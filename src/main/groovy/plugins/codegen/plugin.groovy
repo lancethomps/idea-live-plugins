b6d115b7059616ae30e2631867632d9815dfda2c
Lance Thompson <lthompson05@gmail.com>
2018-11-17 00:02:08 -0800

    misc

--- src/main/groovy/plugins/codegen/plugin.groovy
+++ src/main/groovy/plugins/codegen/plugin.groovy
@@ -1,9 +1,13 @@
 package plugins.codegen
 
 import common.Runner
+import plugins.codegen.all.AllGenerateConstructorAction
+import plugins.codegen.all.AllGenerateGetterAction
 
 // add-to-classpath $HOME/github/idea-live-plugins/src/main/groovy
 
 Runner.isIdeStartup = isIdeStartup
 
+Runner.registerAction(new AllGenerateConstructorAction(), "Java Data Class Code Gen - Constructor", "meta shift C")
+Runner.registerAction(new AllGenerateGetterAction(), "Java Data Class Code Gen - Getters", "meta shift G")
 Runner.registerAction(new JavaDataClassGenAction(), "Java Data Class Code Gen - Constructor & Getters")
--- src/main/groovy/plugins/sandbox/plugin.groovy
+++ src/main/groovy/plugins/sandbox/plugin.groovy
@@ -1,22 +1,11 @@
 package plugins.sandbox
 
-import static liveplugin.PluginUtil.registerAction
 import static liveplugin.PluginUtil.show
 
-import java.lang.reflect.Field
-import java.util.concurrent.ConcurrentMap
-
-import com.intellij.ide.plugins.PluginManager
-import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.impl.ApplicationImpl
 import com.intellij.openapi.editor.colors.impl.AppEditorFontOptions
-import com.intellij.util.messages.Topic
-import com.intellij.util.messages.impl.MessageBusConnectionImpl
-import com.intellij.util.messages.impl.MessageBusImpl
-import com.intellij.util.ui.UIUtil
 
-import common.Logs
-import common.PrefsUtil
 import common.Runner
 
 // add-to-classpath $HOME/github/idea-live-plugins/src/main/groovy
@@ -29,4 +18,9 @@ static def showEditorFontSize() {
   show("Editor Font Size: ${AppEditorFontOptions.getInstance().getState().FONT_SIZE}")
 }
 
-showEditorFontSize()
+static def test() {
+  ApplicationImpl app = (ApplicationImpl) ApplicationManager.getApplication()
+  show("Save allowed: ${app.isSaveAllowed()}")
+}
+
+test()
