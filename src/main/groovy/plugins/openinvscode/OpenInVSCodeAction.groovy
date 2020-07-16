3d912f7a5cb09c5b2d61849ef07748c10a373249
lancethomps <lthompson05@gmail.com>
2020-07-16 14:33:48 -0700

    [git_commit_and_push_all] various updates

--- src/main/groovy/plugins/openinvscode/OpenInVSCodeAction.groovy
+++ src/main/groovy/plugins/openinvscode/OpenInVSCodeAction.groovy
@@ -1,5 +1,7 @@
 package plugins.openinvscode
 
+import java.util.concurrent.TimeUnit
+
 import org.jetbrains.annotations.NotNull
 
 import com.intellij.openapi.actionSystem.AnAction
@@ -7,6 +9,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.vfs.VirtualFile
 
+import common.Logs
 import liveplugin.PluginUtil
 
 class OpenInVSCodeAction extends AnAction {
@@ -15,15 +18,28 @@ class OpenInVSCodeAction extends AnAction {
   void actionPerformed(@NotNull AnActionEvent e) {
     VirtualFile[] files = CommonDataKeys.VIRTUAL_FILE_ARRAY.getData(e.getDataContext());
     if (files != null && files.length > 0) {
+      String codeScriptPath = getCodeScriptPath()
+      Logs.log("Using code script path: ${codeScriptPath}")
       files.each { vfile ->
         File file = new File(vfile.getPresentableUrl())
         if (file.isFile()) {
-          ProcessBuilder processBuilder = new ProcessBuilder("/usr/local/bin/code", "-r", file.getCanonicalPath())
-          processBuilder.start()
+          ProcessBuilder processBuilder = new ProcessBuilder(codeScriptPath, "-r", file.getCanonicalPath())
+          Process process = processBuilder.start()
+          if (process.waitFor(30, TimeUnit.SECONDS) && process.exitValue() != 0) {
+            Logs.log("Error running code script (${process.exitValue()})\nstderr\n${process.getErrorStream().readLines().join("\n")}\nstdout\n${process.getText()}")
+          }
         } else {
           PluginUtil.show("IntelliJ editor file is not an actual file: ${vfile.toString()}")
         }
       }
     }
+  }
+
+  String getCodeScriptPath() {
+    File userFile = new File(System.getProperty("user.home", System.getenv("HOME")) + "/bin/code")
+    if (userFile.exists() && userFile.canExecute()) {
+      return userFile.getCanonicalPath()
+    }
+    return "/usr/local/bin/code"
   }
 }
