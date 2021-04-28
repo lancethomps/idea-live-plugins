af98e73287519975a57830fe8308400645189073
lancethomps <lthompson05@gmail.com>
2021-04-27 19:19:11 -0700

    get rid of RemoveExtraDebuggerKotlinFiltersAction.groovy

--- src/main/groovy/plugins/startup/plugin.groovy
+++ src/main/groovy/plugins/startup/plugin.groovy
@@ -1,29 +1,7 @@
 package plugins.startup
 
-
-import com.intellij.openapi.application.ApplicationManager
-
 import common.Runner
 
 // add-to-classpath $HOME/github/idea-live-plugins/src/main/groovy
 
 Runner.isIdeStartup = isIdeStartup
-
-def runThis = false
-if (runThis) {
-  def removeExtraDebuggerKotlinFiltersAction = new RemoveExtraDebuggerKotlinFiltersAction()
-  Runner.registerAction(removeExtraDebuggerKotlinFiltersAction, "Remove Extra Debugger Kotlin Filters")
-
-  if (isIdeStartup) {
-    ApplicationManager.getApplication().invokeLaterOnWriteThread(
-      new Runnable() {
-
-        @Override
-        void run() {
-          RemoveExtraDebuggerKotlinFiltersAction.logExistingKotlinFiltersCount(false)
-          RemoveExtraDebuggerKotlinFiltersAction.run()
-        }
-      }
-    )
-  }
-}
