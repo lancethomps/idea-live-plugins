cc5dfb191bbc3fe7eb6a1a874986e05ffa82a8c2
lancethomps <lthompson05@gmail.com>
2022-12-01 00:16:13 -0800

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
   java
   groovy
   id("com.github.ben-manes.versions") version "0.42.0"
-  id("org.jetbrains.intellij") version "1.4.0"
-  kotlin("jvm") version "1.6.10"
+  id("org.jetbrains.intellij") version "1.9.0"
+  kotlin("jvm") version "1.6.21"
 }
 
 repositories {
@@ -14,20 +14,20 @@ repositories {
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_11
-  targetCompatibility = JavaVersion.VERSION_11
+  sourceCompatibility = JavaVersion.VERSION_17
+  targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.6.10")
-  implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
-  implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
-  implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:1.6.10")
-  implementation("org.jetbrains.kotlin:kotlin-scripting-common:1.6.10")
-  implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:1.6.10")
-  implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-impl-embeddable:1.6.10")
-  compileOnly("org.codehaus.groovy:groovy-all:2.5.14")
-  compileOnly("liveplugin:live-plugin:0.7.5")
+  implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.6.21")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
+  implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.21")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:1.6.21")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-common:1.6.21")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:1.6.21")
+  implementation("org.jetbrains.kotlin:kotlin-scripting-compiler-impl-embeddable:1.6.21")
+  compileOnly("org.codehaus.groovy:groovy-all:3.0.9")
+  compileOnly("liveplugin:live-plugin:0.8.2")
 }
 
 sourceSets {
@@ -39,7 +39,7 @@ sourceSets {
 }
 
 intellij {
-  val ideVersion = System.getenv().getOrDefault("LIVEPLUGIN_IDEA_VERSION", "2021.3")
+  val ideVersion = System.getenv().getOrDefault("LIVEPLUGIN_IDEA_VERSION", "2022.3")
   println("Using IntelliJ version: ${ideVersion}")
   version.set(ideVersion)
 
@@ -48,7 +48,9 @@ intellij {
   sameSinceUntilBuild.set(false)
   updateSinceUntilBuild.set(false)
 
-  plugins.set(listOf("Groovy", "Kotlin", "git4idea", "github", "junit", "java"))
+  plugins.set(
+    listOf("Groovy", "Kotlin", "Git4Idea", "org.jetbrains.plugins.github", "junit", "java")
+  )
 }
 
 tasks {
@@ -56,6 +58,6 @@ tasks {
   wrapper { distributionType = Wrapper.DistributionType.ALL }
 
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions { jvmTarget = "11" }
+    kotlinOptions { jvmTarget = "17" }
   }
 }
--- src/main/groovy/common/Runner.groovy
+++ src/main/groovy/common/Runner.groovy
@@ -1,11 +1,11 @@
 package common
 
-import static liveplugin.PluginUtil.registerAction
 import static liveplugin.PluginUtil.show
 
 import java.util.regex.Pattern
 
 import com.intellij.openapi.actionSystem.AnAction
+import liveplugin.PluginUtil
 
 class Runner {
 
@@ -16,13 +16,13 @@ class Runner {
   }
 
   static AnAction registerAction(Closure action, String displayText, String keyStroke = "", String actionId = displayText, String actionGroupId = null) {
-    registerAction(actionId, keyStroke, actionGroupId, displayText, action)
+    PluginUtil.registerAction(actionId, keyStroke, actionGroupId, displayText, action)
     if (!isIdeStartup) show(String.format("Loaded %s<br/>%s<br/>Run it through the actions search.", actionId, displayText))
     return action
   }
 
   static AnAction registerAction(AnAction action, String displayText, String keyStroke = "", String actionId = createActionId(action), String actionGroupId = null) {
-    registerAction(actionId, keyStroke, actionGroupId, displayText, action)
+    PluginUtil.registerAction(actionId, keyStroke, actionGroupId, displayText, action)
     if (!isIdeStartup) show(String.format("Loaded %s<br/>%s<br/>Run it through the actions search.", actionId, displayText))
     return action
   }
